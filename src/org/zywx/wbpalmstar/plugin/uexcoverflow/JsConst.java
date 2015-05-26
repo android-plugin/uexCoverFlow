5980e8cb7c02f8b8edc91a9a11322086b0f6cef7
yanlongtao <longtao.yan@3g2win.com>
2015-05-26 18:14:36 +0800

    init

---
+++ src/org/zywx/wbpalmstar/plugin/uexcoverflow/JsConst.java
@@ -0,0 +1,6 @@
+package org.zywx.wbpalmstar.plugin.uexcoverflow;
+
+public class JsConst {
+    public static final String ON_ITEM_PIC_CLICK = "uexCoverFlow.onItemPicClick";
+    public static final String ON_SCROLL_TO_POSITION = "uexCoverFlow.onScrollToPosition";
+}
